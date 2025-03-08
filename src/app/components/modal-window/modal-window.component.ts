a7b54b14b1df4f7f5bfa03d4c1a201cfcfcfca86
Ross Lehr <itsme@rosslehr.com>
2025-03-08 14:21:08 -0500

    Removed Modal width and height to adjust the window position

--- src/app/components/modal-window/modal-window.component.ts
+++ src/app/components/modal-window/modal-window.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild, AfterViewInit } from '@angular/core';
 import { ModalWindowService } from '../../services/modal-window.service';
 import { MainContentComponent } from '../main-content/main-content.component';
 
@@ -9,54 +9,49 @@ import { MainContentComponent } from '../main-content/main-content.component';
   templateUrl: './modal-window.component.html',
   styleUrl: './modal-window.component.scss'
 })
-export class ModalWindowComponent {
+export class ModalWindowComponent implements AfterViewInit {
+
+  @ViewChild('modalContent', { static: false }) modalContent!: ElementRef;
 
   constructor(private modalWindow: ModalWindowService) {}
 
-  fileToLoad: string = ""
+  fileToLoad: string = "";
   divId: string = "";
   modalTitle: string = "";
-  modalHeight: Number = 500;
-  modalWidth: Number = 700;
-
+  //modalHeight: number = 500;
+  //modalWidth: number = 700;
   isVisible = false;
 
   ngOnInit() {
-  
     this.modalWindow.actionTriggered$.subscribe((windowInfo: any) => {
-
       console.log("openModalWindow from modal-window component subscribe");
-
       this.fileToLoad = windowInfo.contentFile;
-
       this.divId = windowInfo.divId;
-
       this.modalTitle = windowInfo.title;
-
-      this.modalHeight = windowInfo.height;
-
-      this.modalWidth = windowInfo.width;
-
       this.openModalWindow();
-
     });
-  
   }
 
-  openModalWindow(): void
-  {
+  ngAfterViewInit() {
+    this.adjustSize();
+  }
 
+  openModalWindow(): void {
     console.log("openModalWindow from modal-window component");
-
     this.isVisible = true;
-
+    setTimeout(() => this.adjustSize(), 100); // Allow content to load before adjusting
   }
 
-  closeModalWindow(): void
-  {
+  closeModalWindow(): void {
     console.log("closeModalWindow from modal-window component");
-
     this.isVisible = false;
   }
 
+  adjustSize(): void {
+    if (this.modalContent) {
+      const contentElement = this.modalContent.nativeElement;
+      //this.modalWidth = contentElement.scrollWidth + 40; // Adding padding
+      //this.modalHeight = contentElement.scrollHeight + 80; // Including header/footer
+    }
+  }
 }
