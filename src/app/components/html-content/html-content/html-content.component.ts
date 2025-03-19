02c6346d041ff76041d7931cb060fe786b4b02a9
Ross Lehr <itsme@rosslehr.com>
2025-03-19 19:37:03 -0400

    fixed imports

--- src/app/components/html-content/html-content/html-content.component.ts
+++ src/app/components/html-content/html-content/html-content.component.ts
@@ -10,11 +10,7 @@ import 'highlight.js/styles/atom-one-dark.css';
 @Component({
   selector: 'app-html-content',
   standalone: true,
-  imports: [
-// TODO: `HttpClientModule` should not be imported into a component directly.
-// Please refactor the code to add `provideHttpClient()` call to the provider list in the
-// application bootstrap logic and remove the `HttpClientModule` import from this component.
-HttpClientModule],
+  imports: [],
   templateUrl: './html-content.component.html',
   styleUrls: ['./html-content.component.scss']
 })
